8ee52dfdbcb6058abc0d38133515b41c39649e6f
Harit85 <pharit85@gmail.com>
2023-09-27 15:08:59 +0700

    remove side bar vue

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,6 @@ import LineOACheck from './components/LineOACheck.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
     <LineOACheck />
   </main>
